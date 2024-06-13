a51d45bea61d43b72169ffbed42320df3daf05d4
LeeMichael24 <leemichaeln24@gmail.com>
2024-06-13 13:24:59 -0700

    Adding doctor Views and chnage Role

--- client-parcial2/src/App.jsx
+++ client-parcial2/src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Layout from './components/layout/layout';
-import Missing from './components/missing/missing';
-import Login from './components/login/login';
-import Register from './components/register/register';
-import HomeUser from './views/user/HomeUser';
-import PacienteHome from './views/paciente/pacienteHome';
-import CrearCitaPaciente from './views/paciente/crearCita/crearCitaPaciente';
-import RecordPaciente from './views/paciente/crearCita/recordPaciente/recordPaciente';
-import RecordDetailPaciente from './views/paciente/crearCita/recordPaciente/recordDetailPaciente/recordDetailPaciente';
-import DoctorHome from './views/doctor/doctorHome';
-import AppointmentList from './views/doctor/appointmentList/apointmentList'
-import AppointmentDetail from './views/doctor/appointmentList/appointmentDetail/AppointmentDetail'
-import CrearCitaDoc from './views/doctor/crearCitaDoc/crearCitaDoc'
-import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './context/AuthProvider';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import Missing from "./components/missing/missing";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+import HomeUser from "./views/user/HomeUser";
+import PacienteHome from "./views/paciente/pacienteHome";
+import CrearCitaPaciente from "./views/paciente/crearCita/crearCitaPaciente";
+import RecordPaciente from "./views/paciente/crearCita/recordPaciente/recordPaciente";
+import RecordDetailPaciente from "./views/paciente/crearCita/recordPaciente/recordDetailPaciente/recordDetailPaciente";
+import DoctorHome from "./views/doctor/doctorHome";
+import AppointmentList from "./views/doctor/appointmentList/apointmentList";
+import AppointmentDetail from "./views/doctor/appointmentList/appointmentDetail/AppointmentDetail";
+import CrearCitaDoc from "./views/doctor/crearCitaDoc/crearCitaDoc";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider } from "./context/AuthProvider";
+import Admin from "./views/admin/admin";
 
 
 function App() {
@@ -38,16 +39,37 @@ function App() {
             {/* Paciente */}
             <Route element={<PrivateRoute requiredRole="USER" />}>
               <Route path="/paciente" element={<PacienteHome />} />
-              <Route path="/paciente/appointment/request" element={<CrearCitaPaciente />} />
-              <Route path="/paciente/user/record" element={<RecordPaciente />} />
-              <Route path="/paciente/user/record/:id" element={<RecordDetailPaciente />} />
+              <Route
+                path="/paciente/appointment/request"
+                element={<CrearCitaPaciente />}
+              />
+              <Route
+                path="/paciente/user/record"
+                element={<RecordPaciente />}
+              />
+              <Route
+                path="/paciente/user/record/:id"
+                element={<RecordDetailPaciente />}
+              />
             </Route>
 
-          {/* doctor */}
-          <Route path="/doctorHome" element={<DoctorHome />} />
-          <Route path="/doctorHome/crearCita" element={<CrearCitaDoc />} />
-          <Route path="/doctorHome/appointmentList" element={<AppointmentList />} />
-          <Route path="/doctorHome/appointmentList/:id" element={<AppointmentDetail />} />
+            {/* doctor */}
+            <Route path="/doctorHome" element={<DoctorHome />} />
+            <Route path="/doctorHome/crearCita" element={<CrearCitaDoc />} />
+            <Route
+              path="/doctorHome/appointmentList"
+              element={<AppointmentList />}
+            />
+            <Route
+              path="/doctorHome/appointmentList/:id"
+              element={<AppointmentDetail />}
+            />
+
+
+              {/* ADMIN */}
+            <Route path="/admin" element={<Admin/>} />
+
+
 
             <Route path="*" element={<Missing />} />
           </Route>
