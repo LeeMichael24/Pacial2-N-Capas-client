2eae22c3995fceb6d74194d8da6c24f7250c38fe
LeeMichael24 <leemichaeln24@gmail.com>
2024-06-15 22:25:08 -0700

    Adding protected route ASST

--- client-parcial2/src/App.jsx
+++ client-parcial2/src/App.jsx
@@ -1,26 +1,24 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Layout from './components/layout/layout';
-import Missing from './components/missing/missing';
-import Login from './components/login/login';
-import Register from './components/register/register';
-import HomeUser from './views/user/HomeUser';
-import PacienteHome from './views/paciente/pacienteHome';
-import CrearCitaPaciente from './views/paciente/crearCita/crearCitaPaciente';
-import RecordPaciente from './views/paciente/crearCita/recordPaciente/recordPaciente';
-import DoctorHome from './views/doctor/doctorHome';
-import AppointmentList from './views/doctor/appointmentList/apointmentList'
-import AppointmentDetail from './views/doctor/appointmentList/appointmentDetail/AppointmentDetail'
-import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './context/AuthProvider';
-import PrescriptionPerId from './views/doctor/prescriptionPerId/prescriptionPerId';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import Missing from "./components/missing/missing";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+import HomeUser from "./views/user/HomeUser";
+import PacienteHome from "./views/paciente/pacienteHome";
+import CrearCitaPaciente from "./views/paciente/crearCita/crearCitaPaciente";
+import RecordPaciente from "./views/paciente/crearCita/recordPaciente/recordPaciente";
+import DoctorHome from "./views/doctor/doctorHome";
+import AppointmentList from "./views/doctor/appointmentList/apointmentList";
+import AppointmentDetail from "./views/doctor/appointmentList/appointmentDetail/AppointmentDetail";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider } from "./context/AuthProvider";
+import PrescriptionPerId from "./views/doctor/prescriptionPerId/prescriptionPerId";
 import Admin from "./views/admin/admin";
 import AssistantHome from "./views/secretaria/assistantHome";
-import CrearRecordAssit from './views/secretaria/crearRecordAsst/crearRecordAssit';
-import FinalizarCita from './views/doctor/finalizarCita/finalizarCita';
-import RecordDoctor from './views/doctor/crearRecord/recordDoctor';
-
-
+import CrearRecordAssit from "./views/secretaria/crearRecordAsst/crearRecordAssit";
+import FinalizarCita from "./views/doctor/finalizarCita/finalizarCita";
+import RecordDoctor from "./views/doctor/crearRecord/recordDoctor";
 
 function App() {
   return (
@@ -41,7 +39,7 @@ function App() {
             <Route path="/dashboard" element={<HomeUser />} />
 
             {/* Paciente */}
-            <Route element={<PrivateRoute allowedRoles={['PSNT']} />}>
+            <Route element={<PrivateRoute allowedRoles={["PSNT"]} />}>
               <Route path="/paciente" element={<PacienteHome />} />
               <Route
                 path="/paciente/appointment/request"
@@ -53,33 +51,45 @@ function App() {
               />
             </Route>
 
-
             {/* ASISTANT */}
-            <Route path="/asistant" element={<AssistantHome />} />
-            <Route
+            <Route element={<PrivateRoute allowedRoles={["ASST"]} />}>
+              <Route path="/asistant" element={<AssistantHome />} />
+              <Route
                 path="/asistant/user/record"
                 element={<CrearRecordAssit />}
-            />
-
-
+              />
+            </Route>
 
             {/* <Route element={<PrivateRoute allowedRoles={['DOCT']} />}> */}
             {/* Admin */}
-            <Route element={<PrivateRoute allowedRoles={['SUDO']} />}>
+            <Route element={<PrivateRoute allowedRoles={["SUDO"]} />}>
               <Route path="/admin" element={<Admin />} />
             </Route>
 
             {/* doctor */}
-            <Route element={<PrivateRoute allowedRoles={['DOCT']} />}> 
+            <Route element={<PrivateRoute allowedRoles={["DOCT"]} />}>
               <Route path="/doctorHome" element={<DoctorHome />} />
-              <Route path="/doctorHome/appointmentList" element={<AppointmentList />} />
-              <Route path="/doctorHome/appointmentDetail/:appointmentId" element={<AppointmentDetail />} />
-              <Route path="/doctorHome/prescriptionPerId" element={<PrescriptionPerId />} />
-              <Route path="/doctorHome/finalizarCita" element={<FinalizarCita />} />
-              <Route path="/doctorHome/crearRecord" element={<RecordDoctor />} />
-              </Route> 
-
-
+              <Route
+                path="/doctorHome/appointmentList"
+                element={<AppointmentList />}
+              />
+              <Route
+                path="/doctorHome/appointmentDetail/:appointmentId"
+                element={<AppointmentDetail />}
+              />
+              <Route
+                path="/doctorHome/prescriptionPerId"
+                element={<PrescriptionPerId />}
+              />
+              <Route
+                path="/doctorHome/finalizarCita"
+                element={<FinalizarCita />}
+              />
+              <Route
+                path="/doctorHome/crearRecord"
+                element={<RecordDoctor />}
+              />
+            </Route>
 
             <Route path="*" element={<Missing />} />
           </Route>
